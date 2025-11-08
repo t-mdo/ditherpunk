3f56b9747b28d53308aa7200aee44f9dc1aa2c7f
Thibault Miranda de Oliveira <thibault.mirandadeoliveira@gmail.com>
2025-11-07 16:13:31 -0800

    add rudimentary image frontend

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,55 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useRef } from "react";
+import init, { add } from "../dither-wasm/pkg/dither_wasm";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+
+  useEffect(() => {
+    init().then(() => {});
+  }, []);
+
+  const handleFileUpload = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const file = e.target?.files?.[0];
+    if (file) {
+      loadImageToCanvas(file);
+    }
+  };
+
+  const loadImageToCanvas = (file: File) => {
+    const img = new Image();
+
+    img.onload = () => {
+      const canvas = canvasRef.current;
+
+      if (!canvas) return;
+
+      canvas.width = img.width;
+      canvas.height = img.height;
+      const ctx = canvas.getContext("2d");
+      if (!ctx) return;
+
+      ctx.drawImage(img, 0, 0);
+
+      const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+      console.log(imageData);
+    };
+
+    img.src = URL.createObjectURL(file);
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="flex flex-col gap-8">
+      <input
+        type="file"
+        id="avatar"
+        name="avatar"
+        accept="image/png, image/jpeg"
+        onChange={handleFileUpload}
+      />
+      <canvas ref={canvasRef} />
+    </div>
+  );
 }
 
-export default App
+export default App;
