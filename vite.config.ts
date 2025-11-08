4bb23b678bb22e3090ad03051efb8b7a5869f87c
Thibault Miranda de Oliveira <thibault.mirandadeoliveira@gmail.com>
2025-11-07 16:13:25 -0800

    add tailwind

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
+import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import topLevelAwait from "vite-plugin-top-level-await";
 import wasm from "vite-plugin-wasm";
 
 export default defineConfig({
-	plugins: [react(), wasm(), topLevelAwait()],
+  plugins: [react(), wasm(), topLevelAwait(), tailwindcss()],
 });
